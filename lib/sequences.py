91cf11783d42a38cd4d5da64e45304b968e4b0b6
Mary Monchari Gichana <marygichana95@gmail.com>
2024-11-30 22:26:42 +0300

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])  
+        return
+    elif length == 1:
+        print([0])  
+        return
+
+    
+    my_list = [0, 1]
+    for _ in range(2, length):
+        my_list.append(my_list[-1] + my_list[-2])
+    print(my_list)  
